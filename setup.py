ff98e8667088765afb6de0ceeb0bf0d3c538bdf3
toolunious <tech@itsallcode.nl>
2021-07-19 13:20:48 +0000

    added pydantic dep

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'boto3']
+requirements = ['Click>=7.0', 'boto3', 'pydantic==1.8.2']
 
 test_requirements = ['pytest>=3', ]
 
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/itsallcode-nl/python_api_machine',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
