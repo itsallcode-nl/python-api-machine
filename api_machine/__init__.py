5cc935147a64a68bdb600bcdfaf67948dd4bced7
toolunious <tech@itsallcode.nl>
2022-03-07 12:20:38 +0000

    Bump version: 0.2.5 → 0.3.0

--- api_machine/__init__.py
+++ api_machine/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Martijn Meijer"""
 __email__ = 'tech@itsallcode.nl'
-__version__ = '0.2.5'
+__version__ = '0.3.0'
 
 
 from .service import Service
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/itsallcode-nl/python_api_machine',
-    version='0.2.5',
+    version='0.3.0',
     zip_safe=False,
 )
