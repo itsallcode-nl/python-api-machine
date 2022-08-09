05698fad8e603560caab6bfe3416459cb5f579a5
toolunious <tech@itsallcode.nl>
2022-08-09 15:07:06 +0000

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/itsallcode-nl/python_api_machine',
-    version='0.8.0',
+    version='0.9.0',
     zip_safe=False,
 )
